a309c82d799dad742fa3ce479beac5ddb7e82438
Befiker <apple@Befikers-MacBook-Pro-6.local>
2026-03-18 10:09:22 +0300

    C2 and C3: Domain model, Repository and Service layer

--- src/main/java/com/shopwave/exception/ProductNotFoundException.java
+++ src/main/java/com/shopwave/exception/ProductNotFoundException.java
@@ -3,9 +3,8 @@ package com.shopwave.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * ID: ATE/6972/14
- */
+// ID: ATE/6972/14
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(String message) {
